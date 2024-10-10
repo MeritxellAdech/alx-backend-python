7d0f2d274069df5f050d4e7ef7c2c5bbeeecfab7
Merit <meritxelladech@gmail.com>
2024-10-10 13:38:44 +0100

    The string representation of a floating number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module implements the to_str function """
+
+
+def to_str(n: float) -> str:
+    """Convert the given float number to string
+    args
+        n:float - The given number
+    Return
+        The string representation of the float
+    """
+    return str(n)
