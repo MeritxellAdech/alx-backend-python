8e9e274c9dbe6175fdee39a2c45d087df46f10c7
Merit <meritxelladech@gmail.com>
2024-10-10 11:21:20 +0100

    Implement annotations on functions and its parameters

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module implements a basic annotations on a function and its args"""
+
+
+def add(a: float, b: float) -> float:
+    """adds two numbers
+    :args:
+        a:float - first number
+        b:float - second number
+    Return
+        The sum of a and b as float"""
+    return a + b
