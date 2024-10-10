6724c82212776efe9c62ff678dda28fcdaee72ea
Merit <meritxelladech@gmail.com>
2024-10-10 13:30:03 +0100

    Floor function returns the whole number from a given number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module implement floor function """
+import math
+
+
+def floor(n: float) -> float:
+    """Rounds n to its whole number
+    args
+        n:float - the given number
+    Return
+        The whole number in n
+    """
+    return (math.floor(n))
