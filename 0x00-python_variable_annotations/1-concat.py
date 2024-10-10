1d4c9485fb954be5819d609d24318b9a2ba5e76d
Merit <meritxelladech@gmail.com>
2024-10-10 11:28:25 +0100

    Concatenate two given strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This modules implements concatenation on two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Puts two strings together
+    args
+        str1:str -  any given text
+        str2:str - any given text
+    Return
+        The combination of str1 and str2
+    """
+    return str1 + str2
